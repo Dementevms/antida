7cf4db37b07f66f437891483afcf1a92e51c2679
Михаил Дементьев <m.dementyev@info74.ru>
2019-12-05 17:23:48 +0500

    Lesson-4: Task-1

---
+++ lesson_4/task_1/index.js
@@ -0,0 +1,22 @@
+function Game(max = 3, tries = 10) {
+  const getRandom = function(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  };
+
+  this.max = max;
+  this.tries = tries;
+  this.random = getRandom(1, max);
+
+  this.turn = function(n) {
+    if (n === this.random) {
+      console.log('Win!!!');
+      return;
+    }
+    n < this.random ? console.log('n <') : console.log('n >');
+    this.tries--;
+    if(this.tries <= 0){
+      console.log('Game Over');
+    }
+  };
+}
